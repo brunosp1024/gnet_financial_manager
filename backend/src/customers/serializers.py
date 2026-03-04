032c47b8d215ac409fcdcf2e4d4203eef05f5637
Bruno Salvador <bruno.pereira@visie.com.br>
2026-03-03 23:06:25 -0300

    refactor: add fields in serialisers

--- backend/src/customers/serializers.py
+++ backend/src/customers/serializers.py
@@ -14,6 +14,7 @@ class CustomerSerializer(PersonSerializer):
             "start_date",
             "birthday",
             "observations",
+            "is_active",
             "created_at",
             "created_by",
             "updated_at",
--- backend/src/employees/serializers.py
+++ backend/src/employees/serializers.py
@@ -11,11 +11,15 @@ class EmployeeSerializer(PersonSerializer):
             "name",
             "address",
             "phone",
+            "cpf",
             "position",
             "modality",
+            "start_date",
+            "observations",
+            "is_active",
             "created_at",
             "created_by",
             "updated_at",
             "updated_by"
         ]
-        read_only_fields = [ "created_at", "created_by", "updated_at", "updated_by"]
+        read_only_fields = ["created_at", "created_by", "updated_at", "updated_by"]
