beb434529ec713cd2b82459226b0e13f79aa1936
Bruno Salvador <bruno.pereira@visie.com.br>
2026-03-04 11:30:13 -0300

    fix: adjusts transaction serializer when not send customer id

--- backend/src/finance/serializers.py
+++ backend/src/finance/serializers.py
@@ -4,7 +4,7 @@ from finance.models import Transaction
 
 
 class TransactionSerializer(AuditSerializerMixin):
-    customer_id = serializers.CharField(source="customer.id")
+    customer_id = serializers.CharField(source="customer.id", required=False, allow_null=True)
     customer_name = serializers.CharField(source="customer.name", read_only=True)
 
 
@@ -25,3 +25,24 @@ class TransactionSerializer(AuditSerializerMixin):
             "updated_by"
         ]
         read_only_fields = ["created_by", "created_at", "customer_name", "updated_at", "updated_by"]
+    
+    def create(self, validated_data):
+        customer_data = validated_data.pop("customer", {})
+        customer_id = customer_data.get("id")
+
+        if customer_id is not None:
+            validated_data["customer_id"] = customer_id
+
+        return Transaction.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        customer_data = validated_data.pop("customer", None)
+
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+
+        if customer_data is not None:
+            instance.customer_id = customer_data.get("id")
+
+        instance.save()
+        return instance
