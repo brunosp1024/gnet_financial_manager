0b3bb65f2f2ae85861932a06cfc55f07196ac0d7
Bruno Salvador <bruno.pereira@visie.com.br>
2026-03-11 22:38:12 -0300

    refactor: update phone field in person model

--- backend/src/core/models/person.py
+++ backend/src/core/models/person.py
@@ -7,7 +7,7 @@ from utils.validators import validate_cpf, validate_phone
 class Person(TimestampableMixin, DeletedMixin):
     name = models.CharField(max_length=200)
     address = models.TextField(blank=True)
-    phone = models.CharField(max_length=20, blank=True, unique=True, validators=[validate_phone])
+    phone = models.CharField(max_length=20, blank=True, null=True, unique=True, validators=[validate_phone])
     cpf = models.CharField(max_length=11, blank=True, validators=[validate_cpf])
     start_date = models.DateField(blank=True, null=True)
     birthday = models.DateField(blank=True, null=True)
