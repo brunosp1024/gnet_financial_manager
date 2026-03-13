ed09cfee3b115c26045a0e9c81dbf1cda5ff3e4d
Bruno Salvador <bruno.pereira@visie.com.br>
2026-03-13 19:30:50 -0300

    refactor: remove unnecessary fields in customer model

--- backend/src/customers/models.py
+++ backend/src/customers/models.py
@@ -3,9 +3,6 @@ from core.models.person import Person
 
 
 class Customer(Person):
-    due_date = models.DateField(blank=True, null=True)
-    is_overdue = models.BooleanField(default=False)
-    monthly_fee = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
 
     class Meta:
         db_table = "customers"
