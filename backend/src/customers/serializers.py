ea881534a6f6599d00af2c77bc6f2383177f4112
Bruno Salvador <bruno.pereira@visie.com.br>
2026-03-13 19:31:48 -0300

    refator: update customer serializers

--- backend/src/customers/serializers.py
+++ backend/src/customers/serializers.py
@@ -1,8 +1,9 @@
+from rest_framework import serializers
 from core.serializers.serializers import PersonSerializer
 from customers.models import Customer
 
 
-class CustomerSerializer(PersonSerializer):
+class CustomerCreateUpdateSerializer(PersonSerializer):
     class Meta:
         model  = Customer
         fields = [
@@ -15,9 +16,50 @@ class CustomerSerializer(PersonSerializer):
             "birthday",
             "observations",
             "is_active",
+            "created_by",
+            "updated_by",
+        ]
+
+class CustomerDetailSerializer(PersonSerializer):
+    is_overdue = serializers.BooleanField(read_only=True)
+
+    class Meta:
+        model  = Customer
+        fields = [
+            "id",
+            "name",
+            "address",
+            "phone",
+            "cpf",
+            "start_date",
+            "birthday",
+            "observations",
+            "is_active",
+            "is_overdue",
             "created_at",
             "created_by",
             "updated_at",
             "updated_by"
         ]
-        read_only_fields = ["created_at", "created_by", "updated_at", "updated_by"]
+
+class CustomerListSerializer(PersonSerializer):
+    is_overdue = serializers.BooleanField(read_only=True)
+
+    class Meta:
+        model  = Customer
+        fields = [
+            "id",
+            "name",
+            "address",
+            "phone",
+            "cpf",
+            "start_date",
+            "birthday",
+            "observations",
+            "is_active",
+            "is_overdue",
+            "created_at",
+            "created_by",
+            "updated_at",
+            "updated_by"
+        ]
